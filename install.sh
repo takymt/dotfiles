bf27b345e23b4154fb82fb5454929f69437dd49f
takuto-yamamoto <takuto.yamamoto.dev@gmail.com>
2026-01-19 19:50:16 +0900

    fix: change vscode default shell profile --> bash

--- install.sh
+++ install.sh
@@ -89,6 +89,10 @@ link_configs() {
     log_step "Linking non-XDG config"
     ln -sfn "$DOTFILES_DIR/.config/czg/cz.config.mjs" "$HOME/cz.config.mjs"
 
+    # TODO: migrate current vscode settings.json
+    # TODO: support non-macOS platforms
+    # ln -sfn "$DOTFILES_DIR/.config/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+
     log_success "All configs linked"
 }
 
