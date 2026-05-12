18d5530a6e1abf289043a8c0f94af8c02124eb73
takymt <takuto.yamamoto.dev@gmail.com>
2026-05-13 01:02:59 +0900

    Add Neovim git keymaps

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -26,6 +26,10 @@ return {
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
       { "<leader><space>", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "Git files" },
+      { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Git status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Git commits" },
+      { "<leader>gb", "<cmd>Telescope git_branches<cr>", desc = "Git branches" },
     },
     opts = {
       defaults = {
@@ -46,7 +50,57 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+    keys = {
+      {
+        "<leader>gg",
+        function()
+          vim.cmd("tabnew")
+          vim.cmd("terminal lazygit")
+          vim.cmd("startinsert")
+        end,
+        desc = "Lazygit",
+      },
+    },
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require("gitsigns")
+
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
+
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "]c", bang = true })
+          else
+            gitsigns.nav_hunk("next")
+          end
+        end, "Next git hunk")
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal({ "[c", bang = true })
+          else
+            gitsigns.nav_hunk("prev")
+          end
+        end, "Previous git hunk")
+
+        map("n", "<leader>hp", gitsigns.preview_hunk, "Preview hunk")
+        map("n", "<leader>hs", gitsigns.stage_hunk, "Stage hunk")
+        map("n", "<leader>hr", gitsigns.reset_hunk, "Reset hunk")
+        map("n", "<leader>hu", gitsigns.undo_stage_hunk, "Undo stage hunk")
+        map("n", "<leader>hb", function()
+          gitsigns.blame_line({ full = true })
+        end, "Blame line")
+        map("n", "<leader>hd", gitsigns.diffthis, "Diff this")
+        map("v", "<leader>hs", function()
+          gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Stage selected hunk")
+        map("v", "<leader>hr", function()
+          gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+        end, "Reset selected hunk")
+      end,
+    },
   },
 
   -- Auto pairs
